ce50bacda4000a3bd86bfe6d3be119730cae9d9a
Arjay <manabaarjay_bsit@plmun.edu.ph>
2025-04-01 21:07:34 +0800

    Completed assignment: Created tables and inserted data

--- scripts/01_create_tables.sql
+++ scripts/01_create_tables.sql
@@ -1,7 +1,3 @@
--- Create database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS schoolDB;
-USE schoolDB;
-
 -- Creating tblStudents table
 CREATE TABLE tblStudents (
     student_id INT PRIMARY KEY AUTO_INCREMENT,
